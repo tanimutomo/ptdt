753bc8fa8c51feb6011e349b5f4ef546d0042436
tanimutomo <tanimutomo@gmail.com>
2021-04-16 10:07:41 +0900

    feat: update

--- ptdt/ptdt.py
+++ ptdt/ptdt.py
@@ -8,15 +8,24 @@ def image(name: str, tensor: torch.Tensor):
         channel_dim = 0
     else:
         raise ValueError("cannot handle tensor which doesn't have 3 or 4 dimension")
+
+    print(f"{name}: {_basic(tensor)}")
     for c in range(tensor.shape[channel_dim]):
         if channel_dim == 1:
             t = tensor[:, c, ...]
         else:
             t = tensor[c, ...]
-        print(f"{name}[{c}]: min={t.min().item():.2f} max={t.max().item():.2f} mean={t.mean().item():.2f}")
+        print(f"  [{c}]: {_stat(t)}")
 
 
 def tensor(name: str, tensor: torch.Tensor):
-    print(f"{name}: min={tensor.min().item():.2f} max={tensor.max().item():.2f} mean={tensor.mean().item():.2f}")
+    print(f"{name}: {_basic(tensor)} {_stat(tensor)}")
 
+
+def _basic(t: torch.Tensor):
+    return f"shape={list(t.shape)} dtype={t.dtype}"
+
+
+def _stat(t: torch.Tensor):
+    return f"min={t.min().item():.4f} max={t.max().item():.4f} mean={t.mean().item():.4f}"
 
