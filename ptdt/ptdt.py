f478d9cd0a1a640c6b5506616e656afed9edbf94
tanimutomo <tanimutomo@gmail.com>
2021-05-15 09:11:00 +0900

    fix: int tensor issue

--- ptdt/ptdt.py
+++ ptdt/ptdt.py
@@ -27,5 +27,6 @@ def _basic(t: torch.Tensor):
 
 
 def _stat(t: torch.Tensor):
+    t = t.to(torch.float64)
     return f"min={t.min().item():.4f} mean={t.mean().item():.4f} max={t.max().item():.4f}"
 
